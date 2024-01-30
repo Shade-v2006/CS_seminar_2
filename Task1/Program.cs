85da69b433a23d7311ee7841d5d714da9fb58054
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-31 02:53:30 +0700

    add lesson2Seminar:task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая принимает на вход трехзначное число и удаляет вторую цифру
+
+int a = 256;
+
+int digitSecond = (a / 100 * 10) + (a % 10);
+Console.WriteLine(digitSecond);
+
+a = 891;
+digitSecond = (a / 100 * 10) + (a % 10);
+Console.WriteLine(digitSecond);
+
