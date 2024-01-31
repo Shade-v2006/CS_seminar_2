55e024adaac34f5a900fa37fc961bb64dc09e5be
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-31 21:26:52 +0700

    add Lesson2Seminar:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,7 +3,7 @@
 int a = 256;
 
 int digitSecond = (a / 100 * 10) + (a % 10);
-Console.WriteLine(digitSecond);
+Console.WriteLine(digitSecond); //вывод значения
 
 // the next variable - следующая переменная
 a = 891;
